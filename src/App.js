bd033e14ec8e68a5206b267438147150e9e8ba86
Allan Daile Vida <allan.vida@opsolutions.biz>
2023-11-06 21:50:12 +0800

    [TodoList] Added : backend logi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
 import { useState } from "react";
-import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todo, setTodo] = useState("");
@@ -15,7 +15,7 @@ function App() {
       todoList={todoList} 
       setTodoList={setTodoList}>
       </Form>
-      <Todo></Todo>
+      <TodoList todoList={todoList}></TodoList>
     </div>
   );
 }
